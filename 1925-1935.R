52b3fe473497412a61361e2e06d0204d807585bd
qknight00 <66799533+qknight00@users.noreply.github.com>
2020-08-06 15:21:36 -0500

    created stack of each individual year then a large stack for all years in the range 1925-1935

--- 1925-1935.R
+++ 1925-1935.R
@@ -21,12 +21,47 @@ ppt1933.files <- list.files("PRISM 1925-1935/PPT/PRISM_ppt_stable_4kmM2_1933_all
 ppt1934.files <- list.files("PRISM 1925-1935/PPT/PRISM_ppt_stable_4kmM2_1934_all_bil/", pattern = "*.bil$", full.names = T)
 ppt1935.files <- list.files("PRISM 1925-1935/PPT/PRISM_ppt_stable_4kmM2_1935_all_bil/", pattern = "*.bil$", full.names = T)
 
+#stack and drop the layers so that there are 12 of them 
+
+ppt1925.stack <- raster::stack(ppt1925.files)
+ppt1926.stack <- raster::stack(ppt1926.files)
+ppt1927.stack <- raster::stack(ppt1927.files)
+ppt1928.stack <- raster::stack(ppt1928.files)
+ppt1929.stack <- raster::stack(ppt1929.files)
+ppt1930.stack <- raster::stack(ppt1930.files)
+ppt1931.stack <- raster::stack(ppt1931.files)
+ppt1932.stack <- raster::stack(ppt1932.files)
+ppt1933.stack <- raster::stack(ppt1933.files)
+ppt1934.stack <- raster::stack(ppt1934.files)
+ppt1935.stack <- raster::stack(ppt1935.files)
+
+ppt1925.stack <- dropLayer(ppt1925.stack,1)
+ppt1926.stack <- dropLayer(ppt1926.stack,1)
+ppt1927.stack <- dropLayer(ppt1927.stack,1)
+ppt1928.stack <- dropLayer(ppt1928.stack,1)
+ppt1929.stack <- dropLayer(ppt1929.stack,1)
+ppt1930.stack <- dropLayer(ppt1930.stack,1)
+ppt1931.stack <- dropLayer(ppt1931.stack,1)
+ppt1932.stack <- dropLayer(ppt1932.stack,1)
+ppt1933.stack <- dropLayer(ppt1933.stack,1)
+ppt1934.stack <- dropLayer(ppt1934.stack,1)
+ppt1935.stack <- dropLayer(ppt1935.stack,1)
 
 # now we read the array of file names in as a stack of rasters (just a bunch) with the 'stack function
-?stack
+
+#first try without stacking the years first
 ppt1925_1935.stack <- raster::stack(c(ppt1925.files,ppt1926.files,ppt1927.files,ppt1928.files,
                                       ppt1929.files,ppt1930.files,ppt1931.files,ppt1932.files,
                                       ppt1933.files,ppt1934.files,ppt1935.files))
+
+#try again after stacking years and removing first layer
+ppt1925_1935.stack <- raster::stack(c(ppt1925.stack,ppt1926.stack,ppt1927.stack,
+                                      ppt1928.stack,ppt1929.stack,ppt1930.stack,
+                                      ppt1931.stack,ppt1932.stack,ppt1933.stack,
+                                      ppt1934.stack,ppt1935.stack))
+
 sp::plot(ppt1925_1935.stack)
-#not able to plot : margins to large 
-#should I be trying to merge these file
+print(ppt1925_1935.stack)
+
+
+#now lets try to do the same thing with tmin / tmax 
